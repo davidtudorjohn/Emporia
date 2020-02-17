71d7712805688f8ea3305eaa17b7a4e1137406d5
David John <davidjohn_atl@yahoo.com>
2020-02-17 10:44:35 -0500

    refactored

--- src/components/CartItem/index.js
+++ src/components/CartItem/index.js
@@ -1,43 +1,31 @@
 import React from "react";
 import "./cartitem.css";
-const CartItem = props => {
-  // const product = {
-  //   id: props.id,
-  //   name: props.name,
-  //   price: props.price,
-  //   quantity: props.quantity,
-  //   image: props.image,
-  //   category: props.category,
-  //   rating: props.rating,
-  //   reviews: props.reviews
-  // };
-
-  return (
-    <div className="cartItem">
-      {/* <Link
+const CartItem = props => (
+  <div className="cartItem">
+    {/* <Link
         className="routerLink"
         to={`/shop/${props.name.replace(/ /g, "-").toLowerCase()}`}
       > */}
-      <img alt="" className="productThumbnail" src={props.image}></img>
-      {/* </Link> */}
-      <h4 className="cartProductName">{props.name}</h4>
-      <div className="cartProductDetails">
-        <h5 className="cartDetails">${props.price}</h5>
-        <p className="cartDetails" id="quantity">
-          Quantity:
-        </p>
-        <div>
-          {" "}
-          <button className="quantityBtn" onClick={props.remove}>
-            -
-          </button>{" "}
-          {props.quantity}{" "}
-          <button className="quantityBtn" onClick={props.add}>
-            +
-          </button>
-        </div>
+    <img alt="" className="productThumbnail" src={props.image}></img>
+    {/* </Link> */}
+    <h4 className="cartProductName">{props.name}</h4>
+    <div className="cartProductDetails">
+      <h5 className="cartDetails">${props.price}</h5>
+      <p className="cartDetails" id="quantity">
+        Quantity:
+      </p>
+      <div>
+        {" "}
+        <button className="quantityBtn" onClick={props.remove}>
+          -
+        </button>{" "}
+        {props.quantity}{" "}
+        <button className="quantityBtn" onClick={props.add}>
+          +
+        </button>
       </div>
     </div>
-  );
-};
+  </div>
+);
+
 export default CartItem;
